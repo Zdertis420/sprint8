2ce40a09243c528d75d5d937dd83cbc302fbaa9b
Zdertis420 <anrobo07@yandex.ru>
2024-11-11 21:52:14 +0300

    Третье ДЗ на пол шишечки

--- app/src/main/java/orc/zdertis420/playlistmaker/SearchActivity.kt
+++ app/src/main/java/orc/zdertis420/playlistmaker/SearchActivity.kt
@@ -1,12 +1,21 @@
 package orc.zdertis420.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var backToMain: ImageButton
+    private lateinit var searchLine: EditText
+    private lateinit var cancel: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,21 @@ class SearchActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        backToMain = findViewById(R.id.back_to_main)
+        searchLine = findViewById(R.id.search_line)
+        cancel = findViewById(R.id.cancel_button)
+
+        backToMain.setOnClickListener(this@SearchActivity)
+
+
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.back_to_main -> finish()
         }
     }
 }
