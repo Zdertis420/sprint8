b5eea6f548b754edd55ec858f06b99756206b260
Zdertis420 <AnRobo07@yandex.ru>
2024-11-30 23:05:19 +0300

    сделал все дз по 9 спринту

--- app/src/main/java/orc/zdertis420/playlistmaker/SearchActivity.kt
+++ app/src/main/java/orc/zdertis420/playlistmaker/SearchActivity.kt
@@ -1,20 +1,97 @@
 package orc.zdertis420.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var backToMain: MaterialToolbar
+    private lateinit var searchLine: EditText
+    private lateinit var cancel: ImageView
+
+    private lateinit var text: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search_activity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        bind()
+    }
+
+    private fun bind() {
+        backToMain = findViewById(R.id.back_to_main)
+        searchLine = findViewById(R.id.search_line)
+        cancel = findViewById(R.id.clear_text)
+
+        backToMain.setOnClickListener(this@SearchActivity)
+        cancel.setOnClickListener(this@SearchActivity)
+
+        searchLine.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                showCancelButton()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                text = searchLine.text.toString()
+            }
+        })
+    }
+
+    private fun showCancelButton() {
+        if (searchLine.text.isNotEmpty()) {
+            cancel.alpha = 1.0F
+        } else {
+            cancel.alpha = 0.0F
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.back_to_main -> finish()
+
+            R.id.clear_text -> {
+                searchLine.text.clear()
+                (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                    .hideSoftInputFromWindow(v.windowToken, 0)
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(
+        outState: Bundle,
+        outPersistentState: PersistableBundle
+    ) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString("User input", text)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        searchLine.setText(savedInstanceState?.getString("User input"))
     }
 }
--- app/src/main/java/orc/zdertis420/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/orc/zdertis420/playlistmaker/SettingsActivity.kt
@@ -1,13 +1,23 @@
 package orc.zdertis420.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var backToMain: MaterialToolbar
+    private lateinit var share: TextView
+    private lateinit var support: TextView
+    private lateinit var eula: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,10 +28,53 @@ class SettingsActivity : AppCompatActivity() {
             insets
         }
 
-        val backToMain: MaterialToolbar = findViewById(R.id.back_to_main)
+        bind()
+    }
 
-        backToMain.setOnClickListener {
-            finish()
+    private fun bind() {
+        backToMain = findViewById(R.id.back_to_main)
+        share = findViewById(R.id.share)
+        support = findViewById(R.id.support)
+        eula = findViewById(R.id.eula)
+
+        backToMain.setOnClickListener(this@SettingsActivity)
+        share.setOnClickListener(this@SettingsActivity)
+        support.setOnClickListener(this@SettingsActivity)
+        eula.setOnClickListener(this@SettingsActivity)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.back_to_main -> finish()
+
+            R.id.share -> startActivity(Intent.createChooser(Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.course)
+                )
+                type = "text/plain"
+            }, null))
+
+            R.id.support -> startActivity(Intent.createChooser(Intent().apply {
+                action = Intent.ACTION_SEND
+                data = Uri.parse(getString(R.string.mailto))
+
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    getString(R.string.subject)
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.text)
+                )
+            }, null))
+
+            R.id.eula -> startActivity(
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.offer)))
+            )
+
+//            R.id.swith_theme -> application.setTheme()
         }
     }
 }
